61aeabfa1279e723c83abc6444664af5dcae7293
Jerry Applegarth <applegarth1630@gmail.com>
2023-06-08 06:19:10 -0700

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,18 @@
 const btn = document.querySelector("#btn-generate");
 const firstPassword = document.querySelector("#first-password");
 const secondPassword = document.querySelector("#second-password");
+const charSymbols = document.querySelector("#symbols");
+const charNumbers = document.querySelector("#numbers");
+const charLowercase = document.querySelector("#lowercase");
+const charUppercase = document.querySelector("#uppercase");
+
+// charSymbols = false;
+// charNumbers = false;
+// charLowercase = false;
+// charUppercase = false;
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const symbols = ["@", "#", "$", "&",];
-
+const symbols = ["@", "#", "$", "&"];
 const characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
 const lowerCaseLetters = characterCodes.map((code) =>
   String.fromCharCode(code)
@@ -21,13 +29,13 @@ const generatePassword = (
   hasUppercase
 ) => {
   const availableCharacters = [
-    ...(hasSymbols ? symbols : []),
     ...(hasNumbers ? numbers : []),
+    ...(hasSymbols ? symbols : []),
     ...(hasLowercase ? lowerCaseLetters : []),
     ...(hasUppercase ? upperCaseLetters : []),
   ];
   let password = "";
-  if(availableCharacters.length === 0) return "";
+  if (availableCharacters.length === 0) return "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * availableCharacters.length);
     password += availableCharacters[randomIndex];
@@ -36,12 +44,33 @@ const generatePassword = (
 };
 
 
+  if (charNumbers.checked) {
+    availableCharacters.hasNumbers = true;
+  }
+
+
+
+if (charSymbols.checked) {
+    availableCharacters.hasSymbols = true;
+  }
+
+  if (charLowercase.checked) {
+    availableCharacters.hasLowercase = true;
+  }
+
+
+  if (charUppercase.checked) {
+    availableCharacters.hasUppercase = true;
+  }
+
 
 btn.addEventListener("click", () => {
-    firstPassword.textContent = generatePassword(10, true, true, true, true);
-    secondPassword.textContent = generatePassword(10, true, true, true, true);
-
+  firstPassword.textContent = generatePassword(
+    6,
+    charNumbers,
+    charSymbols,
+    charLowercase,
+    charUppercase
+  );
+  secondPassword.textContent = generatePassword(6, true, true, true, true);
 });
-
-
-
